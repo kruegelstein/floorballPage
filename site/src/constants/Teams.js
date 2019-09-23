518de866965c0d8da5f730bc3645baf20e82ae80
Christopher Krügelstein <christopher.kruegelstein@nexenio.com>
2019-09-23 08:20:26 +0200

    table

--- site/src/constants/Teams.js
+++ site/src/constants/Teams.js
@@ -1,33 +1,28 @@
 export const Teams = [
   {
-    name: "Floorball Ingolstadt/Nordheim",
-    spiele: 0,
-    diff: "0:0",
-    punkte: 0
-  },
-  {
-    name: "FC Rennsteig Avalanche",
-    spiele: 0,
-    diff: "0:0",
-    punkte: 0
-  },
-  {
     name: "PSV 90 Dessau",
-    spiele: 0,
-    diff: "0:0",
-    punkte: 0
+    spiele: 1,
+    diff: "11:4",
+    punkte: 3
   },
   {
     name: "SC DHfK Leipzig",
-    spiele: 0,
-    diff: "0:0",
-    punkte: 0
+    spiele: 1,
+    diff: "10:5",
+    punkte: 3
   },
   {
+    name: "Unihockey Igels Dresden",
+    spiele: 1,
+    diff: "7:3",
+    punkte: 3
+  },
+
+  {
     name: "SCS Berlin",
-    spiele: 0,
-    diff: "0:0",
-    punkte: 0,
+    spiele: 1,
+    diff: "9:8",
+    punkte: 3,
     own: true
   },
   {
@@ -37,15 +32,21 @@ export const Teams = [
     punkte: 0
   },
   {
-    name: "Unihockey Igels Dresden",
-    spiele: 0,
-    diff: "0:0",
+    name: "USV Halle Saalebiber",
+    spiele: 1,
+    diff: "3:7",
     punkte: 0
   },
   {
-    name: "USV Halle Saalebiber",
-    spiele: 0,
-    diff: "0:0",
+    name: "FC Rennsteig Avalanche",
+    spiele: 1,
+    diff: "5:10",
+    punkte: 0
+  },
+  {
+    name: "Floorball Ingolstadt/Nordheim",
+    spiele: 2,
+    diff: "12:20",
     punkte: 0
   }
 ];
